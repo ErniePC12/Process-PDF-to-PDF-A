33a68bf9fff7710f3dd2e754aeaea40ff3c75a31
Matt F <mfried@mac.com>
2015-02-06 23:01:11 -0500

    More text & cleanup

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # Process-PDF-to-PDF-A
 This is a simple program that will scan for PDF documents and convert them to PDF/A format using Ghost Script.
 
-
 I am running this on a OS X 10.10 Server.  
 
 MacPorts is installed, and I used that to install Ghost Script.
+
+
+# The Story
+
+When posting a document to the US Courts CM/ECF system, you are required to save the PDF as a PDF/A, or Archived PDF.  For reasons not understood, we have been running into an issue where some fonts do not want to get embedded into the PDF.
+
+This work around is set to run automatically to scan a directory, or set of directories, and automatically convert a PDF to a PDF/A-1 format.  This action will also flatten the document as well.
+
+
+## Notes
+I have some settings that are set in a different file, and I reference them in this main file, but because of the sensitive nature of what I do, I can't post them.
--- process_pdf_to_pdf-a.php
+++ process_pdf_to_pdf-a.php
@@ -5,7 +5,7 @@
 	echo date('M d, Y H:i:s');
 	print "\n".$spacer;
 
-    include "settings.php";
+    include_once "settings.php";
 /** If you do not have your own settings files, you can set them here. **/    
     if (!isset($rootPath))
 	   echo $rootPath = "/directory/";
@@ -15,26 +15,29 @@
 /** Ghost Script installed through MacPorts **/
 	$command = "/opt/local/bin/gs -dPDFA -sDEVICE=pdfwrite -dPDFSETTINGS=/default -dNOPAUSE -dQUIET -dBATCH -sOutputFile=";
 	
-	checkVolume($rootPath);
+	checkVolume($rootPath, $smbLogin, $smbPassword, $smbServer, $smbShare);
 
  	$rootDirectory = dirToArray($rootPath);
 	
-	foreach ($rootDirectory as $key => $dirOrders) 
+	if ($rootDirectory != NULL)
 	{
-		$folder = folderToArray($rootPath.$dirOrders);
-		foreach ($folder as $num => $fileName)
-		{
-			if (substr($fileName, 0, 7) != "(PDF-A)")
-			{
-				print $runExec = $command.$rootPath.$dirOrders."/\"(PDF-A) ".$fileName."\" ".$rootPath.$dirOrders."/\"".$fileName."\" \n";
- 				$runMove = "mv ".$rootPath.$dirOrders."/\"".$fileName."\" ".$rootPath.$processedFolder."PDF-A\ Processed/".$dirOrders."/\"".$fileName."\"\n";
-
-/** I'm not activally running this, but if I wanted to, I just uncomment the following lines. **/ 			
-//  				$outputConvert 	= shell_exec($runExec);  // ." 2>&1"
-//  				$outputMove 	= shell_exec($runMove);  // ." 2>&1"
-			}
-		}
-	}
+        foreach ($rootDirectory as $key => $dirOrders) 
+        {
+            $folder = folderToArray($rootPath.$dirOrders);
+            foreach ($folder as $num => $fileName)
+            {
+                if (substr($fileName, 0, 7) != "(PDF-A)")
+                {
+                    print $runExec = $command.$rootPath.$dirOrders."/\"(PDF-A) ".$fileName."\" ".$rootPath.$dirOrders."/\"".$fileName."\" \n";
+                    $runMove = "mv ".$rootPath.$dirOrders."/\"".$fileName."\" ".$rootPath.$processedFolder."PDF-A\ Processed/".$dirOrders."/\"".$fileName."\"\n";
+                
+/** I'm not actively running this, but if I wanted to, I just uncomment the following lines. **/ 			
+//                	$outputConvert 	= shell_exec($runExec);  // ." 2>&1"
+//                	$outputMove 	= shell_exec($runMove);  // ." 2>&1"
+                }
+            }
+        }
+    } else { echo "\n\tDirectory not found\n"; }
 	
 	$end_time = explode(' ', microtime()); $total_time = $end_time[0] + $end_time[1] - $start_time; 
 	print "\n".$spacer;
@@ -42,19 +45,17 @@
 	print "\n".$spacer;
 	
 /******************************************************************************************************/
-function checkVolume($vol)
+function checkVolume($vol, $smbLogin, $smbPassword, $smbServer, $smbShare)
 {
-    include "settings.php";
-	if(!is_dir($vol))
+	if(is_dir($vol))
 	{
 		echo "Mounting Orders Drive\n";
-		echo "osascript -e 'mount volume \"smb://".$smbLogin.":".$smbPassword."@".$smbServer."/".$smbShare."\"' > /dev/null";
 		$outputMount 	= shell_exec("osascript -e 'mount volume \"smb://".$smbLogin.":".$smbPassword."@".$smbServer."/".$smbShare."\"' > /dev/null");
 	}
 }
 /******************************************************************************************************/
 function dirToArray($dir, $orderDIR="") { 
-    if(!is_dir($vol))
+    if(is_dir($dir))
     {
         $result = array(); 
         $cdir = scandir($dir);
